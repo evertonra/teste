2cc2c0b0df32f63f962a566f7ba1049f9b6836d3
evertonra <67119911+evertonra@users.noreply.github.com>
2021-06-13 14:02:47 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    sites: [],
+    site: false,
+  },
+  methods: {
+    fetchSites() {
+      fetch("./api/sites.json")
+      .then(r => r.json())
+      .then(r => {
+        this.sites = r
+      })
+    },
+    fetchSite(id) {
+      fetch(`./api/${id}/dados.json`)
+      .then(r => r.json())
+      .then( r => {
+        this.site = r
+      })
+    },
+    abrirModal(id) {
+      this.fetchSite(id)
+      window.scrollTo({
+        top: this.scrollY,
+        behavior: "smooth",
+      })
+    },
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.site = false;
+    },
+  },
+  created() {
+    this.fetchSites()
+  },
+})
